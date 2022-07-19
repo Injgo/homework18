f59d8e1584eec5f80d3fb80c053cf0cf8795b9fa
Davit Injgia <dato.injgia18@gmail.com>
2022-07-19 16:46:08 +0400

    Done

---
+++ homework18/ImageViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ImageViewController.swift
+//  homework18
+//
+//  Created by Dato on 19.07.22.
+//
+
+import UIKit
+import Foundation
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var Img: UIImageView!
+    private var pressStartTime : TimeInterval = 0.0
+    
+    
+    var pinchGesture = UIPinchGestureRecognizer()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let longPressRecognizer =  UILongPressGestureRecognizer(target: self, action: #selector(longPressed(_:)))
+        Img.addGestureRecognizer(longPressRecognizer)
+        
+        Img.addGestureRecognizer(createSwipeGestureRecognizer(for: .up))
+        Img.addGestureRecognizer(createSwipeGestureRecognizer(for: .down))
+        Img.addGestureRecognizer(createSwipeGestureRecognizer(for: .right))
+        Img.addGestureRecognizer(createSwipeGestureRecognizer(for: .left))
+        
+        
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinchRecognized(_:)))
+        Img.addGestureRecognizer(pinchGestureRecognizer)
+    }
+    
+    
+    
+    @objc func longPressed (_ sender : UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            pressStartTime = NSDate.timeIntervalSinceReferenceDate
+        }
+        else if sender.state == .ended {
+            let duration = NSDate.timeIntervalSinceReferenceDate - pressStartTime
+            if duration < 1 {
+                navigationController?.popViewController(animated: true)
+            }else {
+                UIView.animate(withDuration: 0.25) {
+                    self.Img.isHidden = true
+                   }
+               
+            }
+        }
+    }
+    
+    
+    @objc func pinchRecognized (_ sender: UIPinchGestureRecognizer) {
+        sender.view?.transform = (sender.view?.transform)!.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 1
+        
+        let information = ["color": UIColor.yellow]
+        NotificationCenter.default.post(name: Notification.Name("com.homeWork18.Notification.colorChange"), object: nil, userInfo: information)
+        
+    }
+    
+    
+    
+    private func createSwipeGestureRecognizer(for direction: UISwipeGestureRecognizer.Direction) -> UISwipeGestureRecognizer {
+        let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeGestureRecognizer.direction = direction
+
+        return swipeGestureRecognizer
+    }
+    
+    
+    
+    @objc func didSwipe(_ sender: UISwipeGestureRecognizer) {
+        var frame = Img.frame
+        
+        switch sender.direction {
+            case .up:
+                frame.origin.y -= 50.0
+            case .down:
+                frame.origin.y += 50.0
+            case .left:
+                frame.origin.x -= 50.0
+            case .right:
+                frame.origin.x += 50.0
+            default:
+                break
+            }
+        UIView.animate(withDuration: 0.25) {
+               self.Img.frame = frame
+           }
+        
+        
+    }
+    
+    
+}
--- homework18/ViewController.swift
+++ homework18/ViewController.swift
@@ -9,11 +9,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redCircle: UIView!
+    @IBOutlet weak var blueCircle: UIView!
+    
+    
+    @IBOutlet weak var purpleTriangle: UIView!
+    
+    @IBOutlet weak var blackTriangle: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
+        
+        setUpTriangle(name: purpleTriangle)
+        setUpTriangle(name: blackTriangle)
+        
+        redCircle.layer.cornerRadius = redCircle.frame.width/2
+        redCircle.layer.masksToBounds = true
+        
+        blueCircle.layer.cornerRadius = blueCircle.frame.width/2
+        blueCircle.layer.masksToBounds = true
+        
+        redCircleTap()
+        blueCircleTap()
+        purpleTriangleTap()
+        blackTriangleTap()
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(changeBackgroundColor), name: Notification.Name("com.homeWork18.Notification.colorChange"), object: nil)
 
+    }
+    
+    
+    func redCircleTap(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapGesture(_:)))
+        redCircle.addGestureRecognizer(tap)
+
+    }
+    func blueCircleTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapGesture(_:)))
+        blueCircle.addGestureRecognizer(tap)
+    }
+    
+    func purpleTriangleTap(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapGesture(_:)))
+        purpleTriangle.addGestureRecognizer(tap)
+    }
+    
+    func blackTriangleTap(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapGesture(_:)))
+        blackTriangle.addGestureRecognizer(tap)
+    }
+    
+    
+    @objc func tapGesture(_ gesture: UITapGestureRecognizer){
+        let vc  = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ImageViewController") as! ImageViewController
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    
+    
+    func setUpTriangle(name: UIView){
+            let heightWidth = name.frame.size.width
+               let path = CGMutablePath()
+
+               path.move(to: CGPoint(x: 0, y: heightWidth))
+               path.addLine(to: CGPoint(x:heightWidth/2, y: heightWidth/2))
+               path.addLine(to: CGPoint(x:heightWidth, y:heightWidth))
+               path.addLine(to: CGPoint(x:0, y:heightWidth))
+
+               let shape = CAShapeLayer()
+               shape.path = path
+               
+        if name.tag ==  1 {
+            shape.fillColor = UIColor.purple.cgColor
+        }
+        else {
+            shape.fillColor = UIColor.black.cgColor
+        }
+        name.isUserInteractionEnabled = true
+
+               name.layer.insertSublayer(shape, at: 1)
+           }
+    
+    @objc func changeBackgroundColor(notification: Notification) {
+        if let color = notification.userInfo?["color"] as? UIColor {
+            view.backgroundColor = color
+        }
+        
+    }
 
 }
 
